f59d2affe6f4a7ad38690caca09a9280fe91db86
oscaralonsoo <alonsocamenforte@gmail.com>
2024-04-18 18:20:35 +0200

    Added classes bubble

---
+++ src/Bubble.cpp
@@ -0,0 +1,18 @@
+
+#include "Bubble.h"
+#include "Sprite.h"
+#include "TileMap.h"
+#include <raymath.h>
+/*
+Bubble::Bubble(const Point& p, State s, Look view) :
+	Entity(p, PLAYER_PHYSICAL_WIDTH, PLAYER_PHYSICAL_HEIGHT, PLAYER_FRAME_SIZE, PLAYER_FRAME_SIZE)
+{
+	state = s;
+	look = view;
+	jump_delay = PLAYER_JUMP_DELAY;
+	map = nullptr;
+	score = 0;
+}
+Bubble::~Bubble()
+{
+}*/
---
+++ src/Bubble.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Entity.h"
+#include "TileMap.h"
+
+/*
+enum class State { LAUNCHING };
+enum class Direction { RIGHT, LEFT };
+
+class Bubble : public Entity
+{
+public:
+	Bubble(const Point& p, State s, Direction dir);
+	~Bubble();
+
+private:
+};*/
